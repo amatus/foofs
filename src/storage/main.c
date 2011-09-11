3691bcc7ffef37fa1371eae2204c7d2909555662
David Barksdale <amatus.amongus@gmail.com>
2011-09-10 23:22:51 -0500

    Add legalese to source files.

--- src/storage/main.c
+++ src/storage/main.c
@@ -1,3 +1,21 @@
+/* -*- Mode: C; indent-tabs-mode: t; c-basic-offset: 2; tab-width: 2 -*- */
+/*
+ * main.c
+ * Copyright (C) David Barksdale 2011 <amatus.amongus@gmail.com>
+ * 
+ * foofs is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * foofs is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along
+ * with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #include <dirent.h>
 #include <errno.h>
 #include <fcntl.h>
