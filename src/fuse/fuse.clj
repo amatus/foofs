665fb13aeca6af9c376f395014d09e671cb61c43
David Barksdale <amatus.amongus@gmail.com>
2011-11-02 21:05:17 -0500

    Use posix_spawn/waitpid to mount fusefs under FreeBSD.
    I decided to get this working under FreeBSD using the mount_fusefs
    helper program.
    All that is needed is to run the helper program while leaving the file
    descriptor open, which java.lang.ProcessBuilder does not let you do.
    Since I've got this working for FreeBSD I suppose it won't be too much
    trouble to do the unix domain socket pair trick for Linux.

--- src/fuse/fuse.clj
+++ src/fuse/fuse.clj
@@ -1,6 +1,7 @@
 ;;(ns foofs.fuse
 ;;  (:import (com.sun.jna Function)))
-(import 'com.sun.jna.Function)
+(import '(com.sun.jna Function Pointer StringArray)
+        'com.sun.jna.ptr.IntByReference)
 
 (defmacro def-jna
   [f lib fname params]
@@ -30,7 +31,36 @@
                                   ^Integer mountflags
                                   ^String data])
 
+(def-jna posix_spawn "c" "posix_spawn" [^IntByReference pid
+                                        ^String path
+                                        ^Pointer file_actions
+                                        ^Pointer attrp
+                                        ^StringArray argv
+                                        ^StringArray envp])
+
+(def-jna-errno waitpid "c" "waitpid" [^Integer pid
+                                      ^IntByReference stat_loc
+                                      ^Integer options])
+                                        
+
 (def fd (open "/dev/fuse" 0100002 0))
 
-(mount "foofs" "/mnt" "fuse.foofs" 7
-  (str "fd=" fd ",rootmode=40000,user_id=0,group_id=0"))
+;;(mount "foofs" "/mnt" "fuse.foofs" 7
+;;  (str "fd=" fd ",rootmode=40000,user_id=0,group_id=0"))
+
+(def argv (StringArray. (doto (make-array String 3)
+                          (aset 0 "/usr/sbin/mount_fusefs")
+                          (aset 1 (str fd))
+                          (aset 2 "/mnt"))))
+(def envp (StringArray. (doto (make-array String 2)
+                          (aset 0 "MOUNT_FUSEFS_SAFE=1")
+                          (aset 1 "MOUNT_FUSEFS_CALL_BY_LIB=1"))))
+(def pid (IntByReference.))
+
+(posix_spawn pid "/usr/sbin/mount_fusefs" nil nil argv envp)
+
+(def stat_loc (IntByReference.))
+
+(waitpid (.getValue pid) stat_loc 0)
+
+(close fd)
