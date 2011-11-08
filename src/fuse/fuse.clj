f8d56671b26de3c35a30cd5e21e7aaba520d1364
David Barksdale <amatus.amongus@gmail.com>
2011-11-08 00:11:54 -0600

    Do type checks and type conversions. Type hints don't do that.

--- src/fuse/fuse.clj
+++ src/fuse/fuse.clj
@@ -1,46 +1,98 @@
 ;;(ns foofs.fuse
 ;;  (:import (com.sun.jna Function)))
-(import '(com.sun.jna Function Pointer StringArray)
+(import '(com.sun.jna Function NativeLong Pointer StringArray)
         'com.sun.jna.ptr.IntByReference)
 
+(defmacro assert-args [fnname & pairs]
+  `(do (when-not ~(first pairs)
+         (throw (IllegalArgumentException.
+                  ~(str fnname " requires " (second pairs)))))
+     ~(let [more (nnext pairs)]
+        (when more
+          (list* `assert-args fnname more)))))
+
 (defmacro def-jna
-  [f lib fname params]
-  `(let [func# (Function/getFunction ~lib ~fname)]
+  [f jna-args args & body]
+  `(let [func# (Function/getFunction ~@jna-args)]
      (defn ~f
-       ~params
-       (.invokeInt func# (to-array ~params)))))
+       ~args
+       (.invokeInt func# (to-array (do ~@body))))))
 
-(defmacro def-jna-errno
-  [f lib fname params]
-  `(let [func# (Function/getFunction ~lib ~fname Function/THROW_LAST_ERROR)]
-     (defn ~f
-       ~params
-       (.invokeInt func# (to-array ~params)))))
+(defn pointer?
+  [ptr]
+  (or (nil? ptr) (instance? Pointer ptr)))
 
-(def-jna-errno open "c" "open" [^String path ^Integer flags ^Integer mode])
+(defn int32_t
+  "Converts x to int32_t. For some reason clojure.core/int doesn't."
+  [x]
+  (Integer. (int x)))
 
-(def-jna-errno close "c" "close" [^Integer fd])
+(defn size_t
+  "Converts x to native size_t."
+  [x]
+  ;; on all the machines I've checked size_t is equivalent to unsigned long
+  (NativeLong. (long x)))
 
-(def-jna getuid "c" "getuid" [])
+;; on all the machines I've checked pid_t is equivalent to int32_t
+(def pid_t int32_t)
 
-(def-jna getgid "c" "getgid" [])
+(def-jna open ["c" "open" Function/THROW_LAST_ERROR]
+  [path flags mode]
+  (assert-args open
+    (string? path) "path is a String"
+    (integer? flags) "flags is an integer"
+    (integer? mode) "mode is an integer")
+  [path (int32_t flags) (int32_t mode)])
 
-(def-jna-errno mount "c" "mount" [^String source
-                                  ^String target
-                                  ^String filesystemtype
-                                  ^Integer mountflags
-                                  ^String data])
+(def-jna close ["c" "close" Function/THROW_LAST_ERROR]
+  [fd]
+  (assert-args close
+    (integer? fd) "fd is an integer")
+  [(int32_t fd)])
 
-(def-jna posix_spawn "c" "posix_spawn" [^IntByReference pid
-                                        ^String path
-                                        ^Pointer file_actions
-                                        ^Pointer attrp
-                                        ^StringArray argv
-                                        ^StringArray envp])
+(def-jna c-read ["c" "read" Function/THROW_LAST_ERROR]
+  [fd buf nbyte]
+  (assert-args c-read 
+    (integer? fd) "fd is an integer"
+    (pointer? buf) "buf is a pointer"
+    (integer? nbyte) "nbyte is an integer")
+  [(int32_t fd) buf (size_t nbyte)])
 
-(def-jna-errno waitpid "c" "waitpid" [^Integer pid
-                                      ^IntByReference stat_loc
-                                      ^Integer options])
+(def-jna getuid ["c" "getuid"]
+  [])
+
+(def-jna getgid ["c" "getgid"]
+  [])
+
+;; This is the type signature for Linux, BSD is different.
+(def-jna mount ["c" "mount" Function/THROW_LAST_ERROR]
+  [source target filesystemtype mountflags data]
+  (assert-args mount
+    (string? source) "source is a String"
+    (string? target) "target is a String"
+    (string? filesystemtype) "filesystemtype is a String"
+    (integer? mountflags) "mountflags is an integer"
+    (string? data) "data is a String")
+  [source target filesystemtype (int32_t mountflags) data])
+
+(def-jna posix_spawn ["c" "posix_spawn"]
+  [pid path file_actions attrp argv envp]
+  (assert-args posix_spawn
+    (instance? IntByReference pid) "pid is an IntByReference"
+    (string? path) "path is a string"
+    (pointer? file_actions) "file_actions is a Pointer"
+    (pointer? attrp) "attrp is a Pointer"
+    (every? string? argv) "argv is a sequence of Strings"
+    (every? string? envp) "envp is a sequence of Strings")
+  [pid path file_actions attrp (into-array argv) (into-array envp)])
+
+(def-jna waitpid ["c" "waitpid" Function/THROW_LAST_ERROR]
+  [pid stat_loc options]
+  (assert-args waitpid
+    (integer? pid) "pid is an integer"
+    (instance? IntByReference stat_loc) "stat_loc is an IntByReference"
+    (integer? options) "options is an integer")
+  [pid stat_loc options])
 
 (def fd (open "/dev/fuse" 0100002 0))
 
@@ -54,8 +106,8 @@
   "/usr/sbin/mount_fusefs"
   nil
   nil
-  (into-array ["mount_fusefs" (str fd) "/mnt"])
-  (into-array ["MOUNT_FUSEFS_SAFE=1" "MOUNT_FUSEFS_CALL_BY_LIB=1"]))
+  ["mount_fusefs" (str fd) "/mnt"]
+  ["MOUNT_FUSEFS_SAFE=1" "MOUNT_FUSEFS_CALL_BY_LIB=1"])
 
 (def stat_loc (IntByReference.))
 
